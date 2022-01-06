4d8c27581dd1c0d73d45c428c5c24be0f27cc057
Titans79 <alilasteve15@gmail.com>
2022-01-06 09:11:12 +0300

    Track questions 1 and 2

---
+++ question1.py
@@ -0,0 +1,34 @@
+
+#input
+name = input("Enter name: ")
+type = input("Enter type (old/new): ")
+kilos_picked = input("Enter kilos picked: ")
+
+
+# allowances 
+allowances = 0
+
+if type == "old":
+    allowances = 100
+elif type == "new":
+    allowances = 50
+else:
+    print("Invalid type!")
+
+# bonus
+bonus = 0
+
+if int(kilos_picked) > 20:
+    bonus = 4 * (int(kilos_picked) - 20)
+
+# daily wages
+wages = allowances + (int(kilos_picked) * 7) + bonus
+
+
+# output
+output = [wages, bonus]
+print(output)
+
+
+
+
---
+++ question2.py
@@ -0,0 +1,38 @@
+#input
+customer_name = input("Customer Name: ")
+product = input("Enter Product (X, Y, Z): ")
+units = input("Units bought: ")
+
+# discount
+def discount():
+    discount = 0
+
+    if product == "X":
+        if (int(units) * 80) > 10000:
+            discount = .1 * (int(units) * 80)
+
+    return discount
+
+
+# VAT
+vat = 0
+
+if product == "Y":
+    vat = 0.16 * (int(units) * 95)
+
+
+# pay
+price = 0
+
+if product == "X":
+    price = 80
+elif product == "Y":
+    price = 95
+elif product == "Z":
+    price = 135
+
+pay = price * int(units) - discount() + vat
+
+
+# output
+print(f"Name => {customer_name}, pay => {pay}")
